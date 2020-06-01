f2f90f9b62e147e69eb77eeccf78333049523ff6
ShuvaDev <s.r.shuvadeb@gmail.com>
2020-06-02 00:44:48 +0600

    Updates

---
+++ precache-manifest.6516a321ebf4225d7f9bed6faa80b98c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f55547638d532d1f3ebcbf7b4996327",
+    "url": "/React-Counter-App/index.html"
+  },
+  {
+    "revision": "300924a44b4fbe11346a",
+    "url": "/React-Counter-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "300924a44b4fbe11346a",
+    "url": "/React-Counter-App/static/js/2.77f0cf93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Counter-App/static/js/2.77f0cf93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa9273b65afeb810fcd3",
+    "url": "/React-Counter-App/static/js/main.cd6b9bed.chunk.js"
+  },
+  {
+    "revision": "079e1f21e716cc7e297b",
+    "url": "/React-Counter-App/static/js/runtime-main.7ee4ba8c.js"
+  }
+]);
